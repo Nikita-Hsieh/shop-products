2620f60c8b856d6466ad476184365ab793ff9246
Nikitahh <nikitahsieh03@gmail.com>
2025-02-26 23:59:27 +0800

    Price & Badge

--- script.js
+++ script.js
@@ -24,12 +24,13 @@ products.forEach((product) => {
                 <span class="price">$${product.price}</span>
                 <i class="ri-shopping-bag-line add-cart"></i>
             </div>
-        `
+	`
 
 	productContainer.appendChild(productBox)
 })
 
 //cart
+
 const cartIcon = document.querySelector('#cart-icon')
 const cart = document.querySelector('.cart')
 const cartClose = document.querySelector('#cart-close')
@@ -63,35 +64,74 @@ const addToCart = (productBox) => {
 	const cartBox = document.createElement('div')
 	cartBox.classList.add('cart-box')
 	cartBox.innerHTML = `
-		<img src="${productImgSrc}" alt="cart image" />
-					<div class="cart-detail">
-						<h2 class="cart-product-title">${productTitle}</h2>
-						<span class="cart-price">${productPrice}</span>
-						<div class="cart-quantity">
-							<button id="decrement">-</button>
-							<span class="number">1</span>
-							<button id="increment">+</button>
-						</div>
+	<img src="${productImgSrc}" alt="cart image" />
+				<div class="cart-detail">
+					<h2 class="cart-product-title">${productTitle}</h2>
+					<span class="cart-price">${productPrice}</span>
+					<div class="cart-quantity">
+						<button id="decrement">-</button>
+						<span class="number">1</span>
+						<button id="increment">+</button>
 					</div>
-					<i class="ri-delete-bin-line cart-remove"></i>
+				</div>
+				<i class="ri-delete-bin-line cart-remove"></i>
 	`
 
 	cartContent.appendChild(cartBox)
 
 	cartBox.querySelector('.cart-remove').addEventListener('click', () => {
 		cartBox.remove()
+		updateCartItemCount(-1)
+		updateTotalPrice()
 	})
 
 	cartBox.querySelector('.cart-quantity').addEventListener('click', (event) => {
 		const numberElement = cartBox.querySelector('.number')
-		let quantity = parseInt(numberElement.textContent)
+		const decrementButton = cartBox.querySelector('#decrement')
+		let quantity = numberElement.textContent
 
 		if (event.target.id === 'decrement' && quantity > 1) {
 			quantity--
+			if (quantity === 1) {
+				decrementButton.style.color = '#999'
+			}
 		} else if (event.target.id === 'increment') {
 			quantity++
+			decrementButton.style.color = '#333'
 		}
-
 		numberElement.textContent = quantity
+		updateTotalPrice()
 	})
+
+	updateCartItemCount(1)
+	updateTotalPrice()
+}
+
+// Cart Price
+const updateTotalPrice = () => {
+	const totalPriceElement = document.querySelector('.total-price')
+	const cartBoxs = cartContent.querySelectorAll('.cart-box')
+	let totalPrice = 0
+	cartBoxs.forEach((cartBox) => {
+		const priceElement = cartBox.querySelector('.cart-price')
+		const quantityElement = cartBox.querySelector('.number')
+		const price = priceElement.textContent.replace('$', '')
+		const quantity = quantityElement.textContent
+		totalPrice += price * quantity
+	})
+	totalPriceElement.textContent = `$${totalPrice}`
+}
+
+// CartIcon Chip Number
+let cartItemCount = 0
+const updateCartItemCount = (change) => {
+	cartItemCount += change
+	const cartItemCountBadge = document.querySelector('.cart-item-count')
+	if (cartItemCount > 0) {
+		cartItemCountBadge.style.visibility = 'visible'
+		cartItemCountBadge.textContent = cartItemCount
+	} else {
+		cartItemCountBadge.style.visibility = 'hidden'
+		cartItemCountBadge.textContent = ''
+	}
 }
